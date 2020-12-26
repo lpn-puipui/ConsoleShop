eda2151b2e90c75c2bb065fe6332318d011f92dd
lpn-puipui <972700714@qq.com>
2020-12-26 17:12:43 +0800

    2.读取Excel； 3.用户输入用户名密码与list比较 4.提示登陆成功/失败

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Test {
@@ -11,5 +12,25 @@ public class Test {
         System.out.println("请输入密码：");
         String password=sc.next();
         System.out.println("你输入的密码"+password);
+
+        File file = new File("G:\\Lanqiao\\ConsoleShop\\src\\list.xlsx");
+        ReadExcel readExcel = new ReadExcel();
+        User users[] = readExcel.readExcel(file);
+        for(int i=0;i<users.length;i++)
+        {
+            /*System.out.print(users[i].getUsername());
+            System.out.print("\t"+users[i].getPassword());
+            System.out.print("\t"+users[i].getAddress());
+            System.out.println(users[i].getPhone());
+
+             */
+            if(username.equals(users[i].getUsername())&&password.equals(users[i].getPassword()))
+            {
+                System.out.println("登陆成功");
+                break;
+            }else {
+                System.out.println("登录失败");
+            }
+        }
     }
 }
