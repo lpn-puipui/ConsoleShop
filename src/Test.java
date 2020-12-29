3f566dc6b88b163e075b94044ad66a26307974dd
lpn-puipui <972700714@qq.com>
2020-12-29 20:31:22 +0800

    处理纯数字的密码校验问题，完善登陆成功/失败后的处理问题

--- src/Test.java
+++ src/Test.java
@@ -4,32 +4,33 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         System.out.println("请输入用户名：");
-
-        Scanner sc = new Scanner(System.in);
-        String username = sc.next();
-        System.out.println("你输入的用户名"+username);
-
-        System.out.println("请输入密码：");
-        String password=sc.next();
-        System.out.println("你输入的密码"+password);
-
-        File file = new File("G:\\Lanqiao\\ConsoleShop\\src\\list.xlsx");
-        ReadExcel readExcel = new ReadExcel();
-        User users[] = readExcel.readExcel(file);
-        for(int i=0;i<users.length;i++)
+        //user1	111
+        //user2	user222222
+        //user3	1234.5
+        boolean bool = true;
+        while (bool)
         {
-            /*System.out.print(users[i].getUsername());
-            System.out.print("\t"+users[i].getPassword());
-            System.out.print("\t"+users[i].getAddress());
-            System.out.println(users[i].getPhone());
+            Scanner sc = new Scanner(System.in);
+            String username = sc.next();
+            System.out.println("你输入的用户名"+username);
 
-             */
-            if(username.equals(users[i].getUsername())&&password.equals(users[i].getPassword()))
+            System.out.println("请输入密码：");
+            String password=sc.next();
+            System.out.println("你输入的密码"+password);
+
+            File file = new File("G:\\Lanqiao\\ConsoleShop\\src\\list.xlsx");
+            ReadExcel readExcel = new ReadExcel();
+            User users[] = readExcel.readExcel(file);
+            for(int i=0;i<users.length;i++)
             {
-                System.out.println("登陆成功");
-                break;
-            }else {
-                System.out.println("登录失败");
+                if(username.equals(users[i].getUsername())&&password.equals(users[i].getPassword()))
+                {
+                    System.out.println("登陆成功");
+                    bool=false;
+                    break;
+                }else {
+                    System.out.println("登录失败,请重新登录");
+                }
             }
         }
     }
