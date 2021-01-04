f2342333c856edc19872de7bf99ce03170cf75b8
lpn-puipui <972700714@qq.com>
2021-01-04 17:09:42 +0800

    添加getProductById方法

--- src/ReadProductExcel.java
+++ src/ReadProductExcel.java
@@ -41,6 +41,38 @@ public class ReadProductExcel {
         return products;
     }
 
+    public Product getProductById(String id,InputStream in){
+        Product products[] = null;
+        try {
+            XSSFWorkbook xw = new XSSFWorkbook(in);
+            XSSFSheet xs = xw.getSheetAt(0);
+            for (int j = 1; j <= xs.getLastRowNum(); j++) {
+                XSSFRow row = xs.getRow(j);
+                Product product = new Product();
+                for (int k = 0; k <= row.getLastCellNum(); k++) {
+                    XSSFCell cell = row.getCell(k);
+                    if (cell == null)
+                        continue;
+                    if (k == 0) {
+                        product.setId(this.getValue(cell));
+                    } else if (k == 1) {
+                        product.setName(this.getValue(cell));
+                    } else if (k == 2) {
+                        product.setPrice(Float.valueOf(this.getValue(cell)));//把字符串转Float
+                    } else if (k == 3) {
+                        product.setDesc(this.getValue(cell));
+                    }
+                }
+                if(id.equals(product.getId())){
+                    return product;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     private String getValue(XSSFCell cell) {
         String value;
         CellType type = cell.getCellTypeEnum();
