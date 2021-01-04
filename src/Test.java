eea13b7d92bf9c2a8230191d73b43c2b5b7be090
lpn-puipui <972700714@qq.com>
2021-01-04 17:33:52 +0800

    完成按1继续购买（不完善） 完成按2查看购物车

--- src/Test.java
+++ src/Test.java
@@ -51,6 +51,55 @@ public class Test {
                     inProduct = Class.forName("Test").getResourceAsStream("/product.xlsx");
                     Product product = readProductExcel.getProductById(pId, inProduct);
                     System.out.println("要购买的商品价格：" + product.getPrice());
+                    if(product!=null){
+                        carts[count++] = product;
+                    }
+                    System.out.println("继续购买商品请按1");
+                    System.out.println("查看购物车请按2");
+                    int choose = sc.nextInt();
+                    if(choose == 1){
+                        inProduct = null;
+                        inProduct = Class.forName("Test").getResourceAsStream("/product.xlsx");
+                        readProductExcel = new ReadProductExcel();
+                        products = readProductExcel.getALLProduct(inProduct);
+                        for (Product p : products) {
+                            System.out.print(p.getId());
+                            System.out.print("\t" + p.getName());
+                            System.out.print("\t" + p.getPrice());
+                            System.out.println("\t" + p.getDesc());
+                        }
+                        System.out.println("请输入商品ID把该商品加入购物车");
+                        pId = sc.next();
+                        inProduct = null;
+                        inProduct = Class.forName("Test").getResourceAsStream("/product.xlsx");
+                        product = readProductExcel.getProductById(pId, inProduct);
+                        System.out.println("要购买的商品价格：" + product.getPrice());
+                        /*
+                        数组模拟的购物车
+                         */
+                        if(product!=null){
+                            carts[count++]=product;//添加购物车
+                        }
+                    }else if(choose == 2){
+                         /*
+                        查看购物车
+                        1、购物车是数组
+                        2、既然是数组，我们就可以用for循环来查看
+                         */
+                        System.out.println("当前购物车商品如下：");
+                        for (Product p : carts) {
+                            if(p!=null) {
+                                System.out.print(p.getId());
+                                System.out.print("\t" + p.getName());
+                                System.out.print("\t" + p.getPrice());
+                                System.out.println("\t" + p.getDesc());
+                            }
+                        }
+                        /*
+                        按1结账
+                        按2继续购物
+                         */
+                    }
                     break;
                 }else {
                     System.out.println("登录失败,请重新登录");
